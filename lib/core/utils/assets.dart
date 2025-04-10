85e9f3a9e5e1f344386fa88a4cb6a6d107535b05
ahmed adel <ahmedadel123422@gmail.com>
2025-04-10 03:45:23 +0200

    commit

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,3 @@
+class AppAssets {
+  static String logoSvg = "assets/images/logo.svg";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/core/utils/theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,7 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: AppThemes.darkTheme,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),

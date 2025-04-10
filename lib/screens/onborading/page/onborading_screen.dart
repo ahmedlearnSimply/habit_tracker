f458e5e9a43b2a6016b21ccd352807690f04f45b
ahmed adel <ahmedadel123422@gmail.com>
2025-04-10 06:15:05 +0200

    commit

--- lib/screens/onborading/page/onborading_screen.dart
+++ lib/screens/onborading/page/onborading_screen.dart
@@ -6,6 +6,7 @@ import 'package:gap/gap.dart';
 import 'package:habit_tracker/core/utils/assets.dart';
 import 'package:habit_tracker/core/utils/colors.dart';
 import 'package:habit_tracker/core/utils/textStyle.dart';
+import 'package:habit_tracker/core/widgets/custom_button.dart';
 import 'package:habit_tracker/screens/onborading/model/onboarding_model.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -18,6 +19,8 @@ class OnboradingScreen extends StatefulWidget {
 
 class _OnboradingScreenState extends State<OnboradingScreen> {
   @override
+  PageController pageController = new PageController();
+  int pageIndex = 0;
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
@@ -26,6 +29,13 @@ class _OnboradingScreenState extends State<OnboradingScreen> {
           children: [
             Expanded(
               child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    pageIndex = value;
+                  });
+                },
+                controller: pageController,
+                itemCount: onboardingPages.length,
                 itemBuilder: (context, index) {
                   return Column(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -34,6 +44,8 @@ class _OnboradingScreenState extends State<OnboradingScreen> {
                         flex: 2,
                       ),
                       Container(
+                        width: 200,
+                        height: 200,
                         padding: EdgeInsets.all(20),
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
@@ -41,7 +53,6 @@ class _OnboradingScreenState extends State<OnboradingScreen> {
                         ),
                         child: SvgPicture.asset(
                           onboardingPages[index].image,
-                          height: 40,
                         ),
                       ),
                       Gap(40),
@@ -72,33 +83,62 @@ class _OnboradingScreenState extends State<OnboradingScreen> {
             SizedBox(
               height: 45,
               child: Row(
-                children: [
-                  SmoothPageIndicator(
-                    controller: PageController(),
-                    count: onboardingPages.length,
-                    effect: ExpandingDotsEffect(
-                      dotWidth: 10,
-                      dotHeight: 10,
-                      spacing: 5,
-                      dotColor: Colors.grey,
-                      activeDotColor: AppColors.primaryText,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    if (pageIndex != onboardingPages.length - 1)
+                      TextButton(
+                        onPressed: () {
+                          pageController.jumpToPage(onboardingPages.length - 1);
+                        },
+                        child: Text("تخطي",
+                            style: getSmallStyle(
+                                fontSize: 16, color: AppColors.secondaryText)),
+                      ),
+                    if (pageIndex == onboardingPages.length - 1)
+                      TextButton(
+                        onPressed: () {
+                          pageController.previousPage(
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.ease);
+                        },
+                        child: Text("رجوع",
+                            style: getSmallStyle(
+                                fontSize: 16, color: AppColors.secondaryText)),
+                      ),
+                    SmoothPageIndicator(
+                      controller: pageController,
+                      count: onboardingPages.length,
+                      effect: ExpandingDotsEffect(
+                        dotWidth: 10,
+                        dotHeight: 10,
+                        spacing: 5,
+                        dotColor: AppColors.secondaryText,
+                        activeDotColor: AppColors.primaryText,
+                      ),
                     ),
-                  ),
-                  Spacer(),
-                  // if (pageIndex == onboardingPages.length - 1)
-                  //   CustomButton(
-                  //     width: 100,
-                  //     height: 80,
-                  //     text: "هيا بنا",
-                  //     onPressed: () {
-                  //       AppLocalStorage.cacheData(
-                  //           key: AppLocalStorage.kOnboarding,
-                  //           value: true);
-                  //       pushReplacement(context, const Welcome());
-                  //     },
-                  //   )
-                ],
-              ),
+                    if (pageIndex != onboardingPages.length - 1)
+                      CustomButton(
+                        width: 100,
+                        height: 80,
+                        text: "التالي",
+                        onPressed: () {
+                          pageController.nextPage(
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.ease);
+                        },
+                      ),
+                    if (pageIndex == onboardingPages.length - 1)
+                      CustomButton(
+                        width: 100,
+                        height: 80,
+                        text: "يلا نبدا",
+                        onPressed: () {
+                          pageController.nextPage(
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.ease);
+                        },
+                      ),
+                  ]),
             ),
           ],
         ),

389ee3732dfa8f9a0330e61a66bd13bb2227ee80
ahmed adel <ahmedadel123422@gmail.com>
2025-04-12 06:21:07 +0200

    commit

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/core/services/localStorage/AppLocalStorage.dart';
+import 'package:habit_tracker/core/utils/colors.dart';
+import 'package:habit_tracker/core/utils/textStyle.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,9 +16,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.cardColor,
       appBar: AppBar(
-        title:
-            Text(AppLocalStorage.getCachedData(key: AppLocalStorage.kUserName)),
+        backgroundColor: AppColors.cardColor,
+        elevation: 0,
+        title: Text(
+          "كل عاده تبني اثر يا ${AppLocalStorage.getCachedData(
+            key: AppLocalStorage.kUserName,
+          )}",
+          style: getTitleStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: IconButton(
+          iconSize: 28,
+          onPressed: () {},
+          icon: Icon(
+            Icons.add_circle_outline_rounded,
+          ),
+        ),
       ),
     );
   }
